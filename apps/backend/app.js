88eeb48098a40ee50770b820062deb061161b839
Hamza-benfaddoul <benfaddoul01@gmail.com>
2024-11-04 12:42:59 +0100

    update app api

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -1,5 +1,5 @@
-const express =       require('express')
-const app     =       express()
+const express = require('express')
+const app = express()
 
 const { sendVerificationEmail } = require('./lib/mail.js');
 
@@ -7,11 +7,11 @@ const { logger } = require('./middleware/logEvent.js')
 
 const verifyJWT = require('./middleware/verifyJWT');
 
-const cookieparser =  require('cookie-parser');
-const cors =          require('cors');
+const cookieparser = require('cookie-parser');
+const cors = require('cors');
 
-const port    =       process.env.PORT | 5000
-const corsOptions =   require('./conf/corsOrigins');
+const port = process.env.PORT | 5000
+const corsOptions = require('./conf/corsOrigins');
 const credentials = require('./middleware/credentials');
 
 
@@ -29,24 +29,24 @@ app.use(express.json());
 app.use(cookieparser());
 
 //routes
-app.use('/login',     require('./routes/api/auth/login'));
-app.use("/register",  require('./routes/api/auth/register'));
-app.use('/logout',   require('./routes/api/auth/logout'));
-app.use('/refresh',   require('./routes/api/auth/refresh'));
+app.use('/api/login', require('./routes/api/auth/login'));
+app.use("/api/register", require('./routes/api/auth/register'));
+app.use('/api/logout', require('./routes/api/auth/logout'));
+app.use('/api/refresh', require('./routes/api/auth/refresh'));
 
 // protected routes
 app.use(verifyJWT);
-app.use('/users',     require('./routes/api/user/user'));
+app.use('/api/users', require('./routes/api/user/user'));
 
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send(err.message)
 });
 
-app.all('*', (req, res)=>{
+app.all('*', (req, res) => {
   res.status(404)
-  res.json({error: 404 , message: 'Not Found'})
+  res.json({ error: 404, message: 'Not Found' })
 });
 
 app.listen(port, () => {
