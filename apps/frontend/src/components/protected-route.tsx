906063b8f431016636f28d5421b0bb84017540df
hamza benfaddoul <benfaddoul01@gmail.com>
2025-04-21 20:34:08 +0100

    feat: add nav bar and side bar

--- apps/frontend/src/components/protected-route.tsx
+++ apps/frontend/src/components/protected-route.tsx
@@ -2,10 +2,18 @@ import useAuth from "@/hooks/useAuth";
 import useRefreshToken from "@/hooks/useRefreshToken";
 import React, { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import NavBar from "./nav-bar";
 
 interface ProtectedRouteProps {
   element: React.ReactElement;
 }
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset,
+} from "@/components/ui/sidebar";
+import { SideBar } from "@/components/side-bar";
+import { Separator } from "@/components/ui/separator";
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   const { auth } = useAuth();
@@ -34,16 +42,33 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
         pathname,
       )
     )
-      return <Navigate to="/protected" />;
-    if (
-      auth?.user.isprofilecomplete === true ||
-      pathname == "/complete-profile"
-    ) {
-      return element;
-    } else {
-        return <Navigate to="/complete-profile" />;
-      }
-    } else return <Navigate to="/login" />;
+      return <Navigate to="/dashboard" />;
+    else {
+      return (
+        <SidebarProvider>
+          <div className="flex w-full h-screen overflow-hidden">
+            <SideBar />
+            <SidebarInset className="flex flex-col flex-grow overflow-hidden">
+              <header className="flex bg-sidebar h-16 shrink-0 items-center gap-2 transition-all duration-300 ease-in-out border-b border-border">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator orientation="vertical" className="mr-2 h-4" />
+                </div>
+                {/* <BreadcrumbNav /> */}
+                <div className="flex gap-4 justify-end items-center ml-auto mr-4">
+                  <NavBar />
+                  {/* <ModeToggle /> */}
+                </div>
+              </header>
+              <main className="bg-muted/50 flex-grow overflow-auto  sm:p-4">
+                {element}
+              </main>
+            </SidebarInset>
+          </div>
+        </SidebarProvider>
+      );
+    }
+  } else return <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
